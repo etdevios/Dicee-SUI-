f69339e9fbc786b353d4ee56abc048b019144a55
Eduard Tokarev <etdevios@gmail.com>
2023-01-26 07:50:04 +0300

    Initial Commit

---
+++ Dicee (SUI)/Dicee__SUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee__SUI_App.swift
+//  Dicee (SUI)
+//
+//  Created by Eduard Tokarev on 26.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct Dicee__SUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
